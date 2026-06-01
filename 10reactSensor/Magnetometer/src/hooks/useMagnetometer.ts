20d7d7aa1c6ff2ed967ad3845ad155ceca768459
Aaddityaaa <adityau5090@gmail.com>
2026-06-01 09:27:23 +0530

    Magnetometer sensor

---
+++ 10reactSensor/Magnetometer/src/hooks/useMagnetometer.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Magnetometer } from "expo-sensors"
+import { Platform } from "react-native";
+
+export function useMagnetometer(){
+    const [available, setAvailable] = useState<boolean | null>(null)
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+    const [z, setZ] = useState(0)
+    const [heading, setHeading] = useState(0)
+
+    useEffect(() => {
+        let subscription: {remove : ()=> void} | undefined;
+
+        (async () => {
+            const isAvailable = await Magnetometer.isAvailableAsync()
+            setAvailable(isAvailable)
+
+            if(!isAvailable) return;
+
+            Magnetometer.setUpdateInterval(100)
+
+            subscription = Magnetometer.addListener((data) => {
+                setX(data.x)
+                setY(data.y)
+                setZ(data.z)
+                setHeading(getHeading(data.x, data.y))
+            })
+        })();
+
+        return ()=>subscription?.remove()
+    }, [])
+
+    return { available, x, y, z, heading}
+
+}
+
+// Convert micro tesla into degress
+function getHeading(x: number, y: number){
+    const radians = Platform.OS === 'ios' ? Math.atan2(x, y) : Math.atan2(-x, -y);
+
+    const degrees = (radians * 180)/ Math.PI;
+
+    return (degrees + 360 ) % 360;
+
+}
