b420e0d98f98c4a4e0345030d48d32708da13212
Aaddityaaa <adityau5090@gmail.com>
2026-06-15 16:51:45 +0530

    access gallery using media-labrary

--- 12cameraMediaAudio/src/app/(tabs)/index.tsx
+++ 12cameraMediaAudio/src/app/(tabs)/index.tsx
@@ -1,10 +1,96 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Linking, Button, ActivityIndicator, FlatList } from "react-native";
+import * as MediaLibrary from "expo-media-library"
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image } from "expo-image";
+
 
 export default function Index() {
+  const [permission, requestPermission] = MediaLibrary.usePermissions()
+  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const loadGallery = async () => {
+    if(!permission){
+    return <Text>Chexking for Premissions</Text>
+  }
+
+  if(!permission?.granted){
+    const deniedPermanently = !permission?.canAskAgain
+
+    return (
+      <View style={{flex: 1, justifyContent: "center", gap: 5, padding: 20}}>
+        <Text>
+          We need to access your gallery to show photos and videos
+        </Text>
+
+        {deniedPermanently ? (
+          <>
+          <Text>Access was denied Enable it in settings</Text>
+          <Button title="Open Settings" onPress={() => Linking.openSettings()} />
+        </>): ( <>
+            <Button title="Grant access" onPress={requestPermission} />
+         </>
+        )}
+
+      </View>
+    )
+  }
+
+  setLoading(true)
+  try {
+    const page = await MediaLibrary.getAssetsAsync({
+      first: 20,
+      mediaType: MediaLibrary.MediaType.photo,
+      sortBy: [[MediaLibrary.SortBy.creationTime, false]]
+    })
+    setAssets(page.assets)
+  } catch (error) {
+      console.log("Error  in fetching photos:", error)
+  }finally{
+    setLoading(false)
+  }
+
+  }
+  
+  useEffect(() => {
+    if(permission?.granted){
+      loadGallery()
+    }
+  }, [permission?.granted])
+  
+  if(!permission){
+    return <ActivityIndicator style={{ flex: 1}} />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{color: "tomato"}}>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Text style={{ marginBottom: 8}}>
+          {assets.length} photos . access : {permission.accessPrivileges}
+        </Text>
+
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+          data={assets}
+          keyExtractor={(item) => item.id}
+          numColumns={3}
+          columnWrapperStyle={{ gap: 2}}
+          contentContainerStyle={{ gap: 2}}
+          renderItem={({item}) => (
+            <Image
+            source={{ uri: item.uri}}
+            style={{ flex: 1, aspectRatio: 1, borderRadius: 4}}
+            contentFit="cover"
+            />
+          )}
+          />
+        )
+      }
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,4 +102,7 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     color: "tomato",
   },
+  text: {
+    color: "tomato"
+  }
 });
